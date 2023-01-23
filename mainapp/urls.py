3b44ea96b8b0dd3e369f11a6659fdabfa23af35b
Polina Nechaeva <polina2000_21@mail.ru>
2023-01-23 23:49:48 +0300

    the table of colors

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from mainapp.views import MainListView
+from mainapp.views import MainListView, ColorListView
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', MainListView.as_view(), name='index'),
+    path('colors/', ColorListView.as_view(), name='color'),
 ]
